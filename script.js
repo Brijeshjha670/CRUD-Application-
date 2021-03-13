0d0aa970db9bfdfc4b1d5a088842fdca890fa2d1
Brijeshjha670 <brijeshjha18011997@gmail.com>
2021-03-12 22:29:07 -0800

    creating css part

--- script.js
+++ script.js
@@ -2,34 +2,33 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
-const path  = require('path');
+const path = require('path');
 
 const app = express();
 
-//declare the path and inform the express to use env file 
-dotenv.config({path:'config.env'})
-const PORT = process.env.PORT || 8080
+//declare the path and inform the express to use env file
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080;
 
-//log requests 
+//log requests
 app.use(morgan('tiny'));
 
 //parse requests to body-parser
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 
 //set view engines
-app.set("view engine","ejs");
+app.set('view engine', 'ejs');
 
 //for reference if you are using and other folder other than views for ejs file
 //app.set("views",path.resolve(__dirname,"views/ejs"))
 
 //load assets
-app.use('/CSS',express.static(path.resolve(__dirname,"assets/CSS")));
-app.use('/img',express.static(path.resolve(__dirname,"assets/img")));
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")));
-
+app.use('/CSS', express.static(path.resolve(__dirname, 'assets/CSS')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
 
 app.get('/', (req, res) => {
-  res.send('Crud Application');
+  res.render('index');
 });
 
 app.listen(PORT, () => {
